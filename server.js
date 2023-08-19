e7cb3667bc151f76e69fcf9bf93b58d8bd3e2b2e
Si Hang Sean NG <21771256@students.ltu.edu.au>
2023-08-19 19:59:43 +1000

    Added Date in a new line

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const app = express();
 
 // Set handler for the index of the website
 app.get('/', (req, res) => {
+    // Adding date for ex4
+    var currentDate = new Date().toLocaleString(); // Get current date and time as a string
     // Run the system `fortune` command and respond with the message
     child_process.exec('fortune', (error, message) => {
         if(error === null) {
-        res.send(message);
+            res.send(message + '<br>' +currentDate);
         } else {
-    res.send('Error: ' + error);
+            res.send('Error: ' + error);
         }
     })
 }); 
